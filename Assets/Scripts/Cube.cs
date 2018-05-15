c95d7918d7aaee719ba0ef0f20b82d0a67a06053
Paul Home <peg3268@g.rit.edu>
2018-05-15 17:03:02 -0500

    Adding all existing files

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+    public int health;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate((Input.GetAxis("Horizontal")*Time.deltaTime * 3),0f,0f);
+        transform.Translate(0f,0f,(Input.GetAxis("Vertical"))*Time.deltaTime * 3);
+	}
+}
