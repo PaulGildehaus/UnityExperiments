b3dfe84f205a00828bc9ce8657930d1a360f0bf2
Paul <pegildy@gmail.com>
2018-05-16 22:43:42 -0400

    Pushing just to work on my desktop

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -9,8 +9,13 @@ public class Sphere : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //iTween.MoveUpdate(this.gameObject, new Vector3(0f, 0.5f, 3f), 3f);
-        iTween.MoveBy(this.gameObject, iTween.Hash("x", 2, "time", 2.5f));
+        currentVector = new Vector3();
+        queue = new Queue<Vector3>();
+        queue.Enqueue(new Vector3(3f, .5f, 0f));
+        queue.Enqueue(new Vector3(0f, .5f, 3f));
+        queue.Enqueue(new Vector3(-3f, .5f, 0f));
+        queue.Enqueue(new Vector3(0f, .5f, -3f));
+
     }
 
 
@@ -25,32 +30,18 @@ public class Sphere : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        //iTween.MoveTo(gameObject, new Vector3(0f, .5f, 5f), 2f);
 
-        /*
+        
         Vector3 currVec = queue.Peek();
-        //print("Queue: " + currVec.ToString() + " | Position: " + currentVector.ToString());
+        print(currVec + "   |   " + currentVector);
         if (currentVector.Equals(currVec))
         {
             queue.Dequeue();
-            if(currVec.x > 0)
-            {
-                transform.Translate(-3f,0f,3f);
-            }
-            else if(currVec.x < 0)
-            {
-                transform.Translate(3f, 0f, -3f);
-            }
-            else if(currVec.z > 0)
-            {
-                transform.Translate(-3f, 0f, -3f);
-            }
-            else if(currVec.z < 0)
-            {
-                transform.Translate(3f, 0f, 3f);
-            }
+            iTween.MoveTo(gameObject, currVec, 3f);
             queue.Enqueue(currVec);
         }
         currentVector = transform.position;
-	*/
+        
     }
 }
---
+++ Assets/Scripts/testItween.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testItween : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("x", 4f, "time", 3f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
