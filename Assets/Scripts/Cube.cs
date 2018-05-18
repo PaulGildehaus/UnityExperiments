195fd54097b978fac1d5588871a88415f512026e
Paul Home <peg3268@g.rit.edu>
2018-05-18 10:14:16 -0500

    Pushing to work on my laptop

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour {
-    public int health;
+    public int lives;
+    public float moveSpeed;
+    public float maxSpeed;
+    private Vector3 input;
 
 	// Use this for initialization
 	void Start () {
-
+        lives = 3;
 	}
 	
     void OnTriggerEnter(Collider col)
@@ -15,14 +18,22 @@ public class Cube : MonoBehaviour {
         if(col.name == "Sphere")
         {
             print("Don't touch that");
+            lives--;
         }
     }
 
 
 	// Update is called once per frame
 	void Update () {
-        transform.Translate((Input.GetAxis("Horizontal")*Time.deltaTime * 3),0f,0f);
-        transform.Translate(0f,0f,(Input.GetAxis("Vertical"))*Time.deltaTime * 3);
+        //transform.Translate((Input.GetAxis("Horizontal")*Time.deltaTime * 3),0f,0f);
+        //transform.Translate(0f,0f,(Input.GetAxis("Vertical"))*Time.deltaTime * 3);
+
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        if (GetComponent<Rigidbody>().velocity.magnitude < maxSpeed)
+        {
+            GetComponent<Rigidbody>().AddForce(input * moveSpeed);
+        }
 
         if(CheckOutOfBounds())
         {
